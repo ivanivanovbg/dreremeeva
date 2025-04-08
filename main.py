9c9a01e16c3a9a328141673170798ef8f2071fda
Ivan Ivanov <i.ivanov.bg@abv.bg>
2025-04-08 11:22:35 +0300

    Initial

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-pass
+import datetime
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/schedule/')
+def get_schedule():
+    month = datetime.datetime.now().month
+    return "Schedule"
